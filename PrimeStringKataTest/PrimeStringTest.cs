0a64ee5c9dff6cb0ca12faee3676266b81e4e8e1
ClaudeLin <linc27595@gmail.com>
2017-07-24 12:38:00 +0800

    Init project

---
+++ PrimeStringKataTest/PrimeStringTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PrimeStringKataTest
+{
+	[TestClass]
+	public class PrimeStringTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
